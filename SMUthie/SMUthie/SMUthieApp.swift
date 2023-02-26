3961d26e48c8dcc943711f8e4c4293c8f1e5ee5c
김유빈 <yubin6894@gmail.com>
2023-02-26 21:46:19 +0900

    [Chore] add branch dev

---
+++ SMUthie/SMUthie/SMUthieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMUthieApp.swift
+//  SMUthie
+//
+//  Created by 김유빈 on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SMUthieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
