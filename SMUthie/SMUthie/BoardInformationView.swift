5955ee7082d62c1fde4a959bcc3f8d18f0ae301b
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-07-17 21:34:19 +0900

    [Feat] BoardPage 구현 시작 , BoardInformationView UI 구현

---
+++ SMUthie/SMUthie/BoardInformationView.swift
@@ -0,0 +1,164 @@
+//
+//  BoardInformationView.swift
+//  SMUthie
+//
+//  Created by Phil on 2023/07/16.
+//
+
+import SwiftUI
+
+struct BoardInformationView: View {
+    @State private var isInfOpened = false
+    @State private var isLiked = false
+    
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 25)
+                .strokeBorder(Color("LightGray"))
+                .background(.white)
+            VStack(spacing: 10){
+                Button(action: {isInfOpened.toggle()}) {
+                    RoundedRectangle(cornerRadius: 50)
+                        .fill(Color("CustomOrange"))
+                        .frame(height: 50)
+                        .overlay(
+                            HStack{
+                                Spacer()
+                                Spacer()
+                                Text("영업정보 보기")
+                                    .foregroundColor(.white)
+                                    .underline()
+                                Spacer(minLength: 80)
+                                Image(systemName: isInfOpened ? "chevron.down" : "chevron.up" )
+                                    .font(.system(size: 20))
+                                    .foregroundColor(.white)
+                                    .padding()
+                                
+                            }
+                        )
+                }.buttonStyle(PlainButtonStyle())
+                if isInfOpened {
+                    
+                    RoundedRectangle(cornerRadius: 5)
+                        .strokeBorder(Color("LightGray"))
+                        .frame(height: 70)
+                        .background(.white)
+                        .overlay(
+                            VStack{
+                                HStack(spacing: 2){
+                                    Image(systemName: "clock")
+                                    Text("이용시간")
+                                        .fontWeight(.bold)
+                                    Spacer()
+                                }
+                                .padding(.horizontal)
+                                Text("09: 00 ~ 21: 00")
+                                    .padding(.bottom)
+                            }
+                        )
+                        .padding(.horizontal)
+                    RoundedRectangle(cornerRadius: 5)
+                        .strokeBorder(Color("LightGray"))
+                        .frame(height: 70)
+                        .background(.white)
+                        .overlay(
+                            VStack{
+                                HStack(spacing: 2){
+                                    Image(systemName: "phone")
+                                    Text("전화번호")
+                                        .fontWeight(.bold)
+                                    Spacer()
+                                }
+                                .padding(.horizontal)
+                                Text("02 - 287 - 1384")
+                                    .padding(.bottom)
+                            }
+                        )
+                        .padding(.horizontal)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 5)
+                            .strokeBorder(Color("LightGray"))
+                            .frame(height: 150)
+                            .background(.white)
+                            .padding(.horizontal)
+                        VStack{
+                            HStack{
+                                Text("메뉴")
+                                    .fontWeight(.bold)
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                            HStack{
+                                Spacer()
+                                Text("부대찌개")
+                                
+                                Spacer()
+                                Spacer()
+                                Text("5000원")
+                                    .foregroundColor(Color("CustomOrange"))
+                                Button(action: {isLiked.toggle()}){
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .strokeBorder(Color("LightGray"))
+                                        .frame(width: 60,height: 30)
+                                        .background(.white)
+                                        .overlay (
+                                            HStack {
+                                                Image (systemName: isLiked ? "heart.fill" : "heart" )
+                                                Text("21")
+                                            }.foregroundColor(Color("CustomOrange"))
+                                        )
+                                }
+                                .padding(.horizontal,10)
+                            }
+                            
+                            HStack{
+                                Spacer()
+                                Text("순두부찌개")
+                                    
+                                Spacer()
+                                Spacer()
+                                Text("5000원")
+                                    .foregroundColor(Color("CustomOrange"))
+                                Button(action: {}){
+                                    Text("버튼")
+                                }
+                                .padding(.horizontal)
+                            }
+                            
+                            HStack{
+                                Spacer()
+                                Button(action: {}){
+                                    Text ("더보기")
+                                        .font(.system(size: 12))
+                                        .foregroundColor(.black)
+                                        .underline()
+                                        
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    HStack{
+                        NavigationLink(destination: {}) {
+                            HStack(spacing: 0){
+                                Image("HandPalm")
+                                Text("제보하러가기")
+                                    .underline()
+                                    .foregroundColor(.black)
+                            }
+                        }.padding()
+                        Spacer()
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+}
+
+struct BoardInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardInformationView()
+    }
+}
--- SMUthie/SMUthie/BoardView.swift
+++ SMUthie/SMUthie/BoardView.swift
@@ -13,86 +13,88 @@ struct BoardView: View {
 
     
     var body: some View {
-        ScrollView{
-            VStack{
-                Text("리뷰게시판")
-                    .font(.system(size: 24))
-                    .foregroundColor(Color("CustomOrange"))
-                    .padding(1)
-                Text("자유롭게 리뷰를 달아주세요 :)")
-                    .font(.system(size: 15))
-                    .foregroundColor(Color("LightGray"))
-                HStack {
-                    Spacer()
-                    Button(action: {isCafeSelected = false}){
-                        RoundedRectangle(cornerRadius: 5)
-                            .strokeBorder(Color("BorderLine"))
-                            .frame(width: 80,height: 30)
-                            .background(isCafeSelected ? .white : Color("BorderLine"))
-                            .cornerRadius(5)
-                            .overlay(
-                                Text("음식점")
-                                    .foregroundColor(Color("CustomOrange"))
-                            )
+        NavigationView {
+            ScrollView{
+                VStack{
+                    Text("리뷰게시판")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color("CustomOrange"))
+                        .padding(1)
+                    Text("자유롭게 리뷰를 달아주세요 :)")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color("LightGray"))
+                    HStack {
+                        Spacer()
+                        Button(action: {isCafeSelected = false}){
+                            RoundedRectangle(cornerRadius: 5)
+                                .strokeBorder(Color("BorderLine"))
+                                .frame(width: 80,height: 30)
+                                .background(isCafeSelected ? .white : Color("BorderLine"))
+                                .cornerRadius(5)
+                                .overlay(
+                                    Text("음식점")
+                                        .foregroundColor(Color("CustomOrange"))
+                                )
+                        }
+                        Button(action: {isCafeSelected = true}){
+                            RoundedRectangle(cornerRadius: 5)
+                                .strokeBorder(Color("BorderLine"))
+                                .frame(width: 80,height: 30)
+                                .background(isCafeSelected ? Color("BorderLine") : .white)
+                                .cornerRadius(5)
+                                .foregroundColor(isCafeSelected ? Color("BorderLine") : .white )
+                                .overlay(
+                                    Text("카페")
+                                        .foregroundColor(Color("CustomOrange"))
+                                )
+                            
+                        }
                     }
-                    Button(action: {isCafeSelected = true}){
-                        RoundedRectangle(cornerRadius: 5)
-                            .strokeBorder(Color("BorderLine"))
-                            .frame(width: 80,height: 30)
-                            .background(isCafeSelected ? Color("BorderLine") : .white)
-                            .cornerRadius(5)
-                            .foregroundColor(isCafeSelected ? Color("BorderLine") : .white )
-                            .overlay(
-                                Text("카페")
-                                    .foregroundColor(Color("CustomOrange"))
-                            )
-                        
-                    }
+                    .padding(.horizontal)
+                    RoundedRectangle(cornerRadius: 5)
+                        .strokeBorder(Color("BorderLine"))
+                        .background(.white)
+                        .frame(minHeight: 50)
+                        .overlay(
+                            HStack {
+                                Text("#학교식당")
+                                    .font(.system(size: 16))
+                                    .foregroundColor(Color("CustomBlue"))
+                                    .padding(.horizontal)
+                                Button(action: {}){
+                                    Text("학식 게시판")
+                                        .font(.system(size: 16))
+                                        .foregroundColor(Color("CustomGray"))
+                                        .underline()
+                                }
+                                .padding(.horizontal)
+                            }
+                        )
+                        .padding(.horizontal)
+                    
+                    RoundedRectangle(cornerRadius: 5)
+                        .strokeBorder(Color("BorderLine"))
+                        .background(.white)
+                        .frame(minHeight: 50)
+                        .overlay(
+                            HStack {
+                                Text("#부대찌개")
+                                    .font(.system(size: 16))
+                                    .foregroundColor(Color("CustomBlue"))
+                                    .padding(.horizontal)
+                                NavigationLink(destination: BoardPageView()) {
+                                    Text("부대통령 게시판")
+                                        .font(.system(size: 16))
+                                        .foregroundColor(Color("CustomGray"))
+                                        .underline()
+                                }
+                                .padding(.horizontal)
+                            }
+                        )
+                        .padding(.horizontal)
                 }
-                .padding(.horizontal)
-                RoundedRectangle(cornerRadius: 5)
-                    .strokeBorder(Color("BorderLine"))
-                    .background(.white)
-                    .frame(minHeight: 50)
-                    .overlay(
-                        HStack {
-                            Text("#학교식당")
-                                .font(.system(size: 16))
-                                .foregroundColor(Color("CustomBlue"))
-                                .padding(.horizontal)
-                            Button(action: {}){
-                                Text("학식 게시판")
-                                    .font(.system(size: 16))
-                                    .foregroundColor(Color("CustomGray"))
-                                    .underline()
-                            }
-                            .padding(.horizontal)
-                        }
-                    )
-                    .padding(.horizontal)
-                
-                RoundedRectangle(cornerRadius: 5)
-                    .strokeBorder(Color("BorderLine"))
-                    .background(.white)
-                    .frame(minHeight: 50)
-                    .overlay(
-                        HStack {
-                            Text("#부대찌개")
-                                .font(.system(size: 16))
-                                .foregroundColor(Color("CustomBlue"))
-                                .padding(.horizontal)
-                            Button(action: {}){
-                                Text("부대통령 게시판")
-                                    .font(.system(size: 16))
-                                    .foregroundColor(Color("CustomGray"))
-                                    .underline()
-                            }
-                            .padding(.horizontal)
-                        }
-                    )
-                    .padding(.horizontal)
+                .fontWeight(.semibold)
             }
-            .fontWeight(.semibold)
         }
     }
 }
