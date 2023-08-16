174d264356361f98fc6e87caf742bc7bcb564505
김유빈 <yubin6894@gmail.com>
2023-08-16 18:48:40 +0900

    [Feat] 회원탈퇴 일부 구현
    문구, 버튼 구현 > 연결 화면 필요

--- SMUthie/SMUthie/MyPageView/WithdrawalView.swift
+++ SMUthie/SMUthie/MyPageView/WithdrawalView.swift
@@ -10,10 +10,45 @@ import SwiftUI
 struct WithdrawalView: View {
     var body: some View {
         VStack {
-            Spacer()
             Text("그동안 슴우디와 함께해주셔서 감사합니다 :) \n\n더이상 슴우디를 이용하지 \n않으시나요?")
                 .font(.system(size: 24))
+                .bold()
+                .padding(.top, 100)
+            
             Spacer()
+            
+            Button(action: {
+                
+            }) {
+                Text("아니요, 계속 이용할래요!")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .bold()
+                    .frame(width: 365, height: 50)
+                    .background(.orange)
+                    .cornerRadius(5)
+                    .shadow(
+                        color: .gray.opacity(0.3),
+                        radius: 1,
+                        x: 3, y: 3)
+                
+            }
+            Button(action: {
+                
+            }) {
+                Text("네, 탈퇴하고 싶습니다.")
+                    .foregroundColor(.orange)
+                    .font(.system(size: 18))
+                    .bold()
+                    .frame(width: 365, height: 50)
+                    .background(.white)
+                    .cornerRadius(5)
+                    .shadow(
+                        color: .gray.opacity(0.3),
+                        radius: 1,
+                        x: 3, y: 3)
+            }
+            
         }
     }
 }
