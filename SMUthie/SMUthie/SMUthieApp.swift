6132124ff2a359c49f40cf7346bfc7302e328ef6
김유빈 <yubin6894@gmail.com>
2023-11-16 13:36:27 +0900

    [Fix] 에러 해결 merge 하면서 수정된 파일때문에 테스트 불가능한 코드 수정

--- SMUthie/SMUthie/SMUthieApp.swift
+++ SMUthie/SMUthie/SMUthieApp.swift
@@ -13,24 +13,6 @@ struct SMUthieApp: App {
         WindowGroup {
             ContentView()
                 .environment(\.font, Font.custom("NanumSquareRoundOTF", size: 16))
-                .onAppear{
-                    NetworkManager.shared.fetchCafe()
-                    NetworkManager.shared.fetchAndamiro()
-                    NetworkManager.shared.postRegister(studentId: "201910790", password: "qwer1234", nickname: "nini")
-                    NetworkManager.shared.postLogin(studentId: "201910790", password: "qwer1234") { result in
-                        switch result {
-                        case .success(let loginResult):
-                            // 로그인 성공 시의 처리
-                            print("Access Token: \(loginResult.access_token)")
-                            print("Refresh Token: \(loginResult.refresh_token)")
-
-                        case .failure(let error):
-                            // 로그인 실패 또는 오류 처리
-                            print("Login Error: \(error.localizedDescription)")
-                        }
-                    }
-
-                }
         }
     }
 }
